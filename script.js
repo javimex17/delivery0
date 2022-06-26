4c91bac184b3a714ad81348237332c6dee4411e1
Javier <javier.garcia@techni-web.mx>
2022-06-26 10:05:19 -0500

    maquetación, array, object

--- script.js
+++ script.js
@@ -1,92 +1,57 @@
+// Inicializo las variables
+let titulo = "PRODUCTOS PARA TU TIENDA ON LINE\r\n____";
+let totalProductos  = 5;
+let totalCompra     = 0;
+const arrayProducto = [];       // -> Array que contiene la lista de productos
+let respuesta       = "SI";
+let contadorID      = 0;
+let opcion          = 1;
 
+// Definición del objeto y su clase con constructor
+class Producto { 
+    constructor (id, nombre, precio) {
+        this.id = id,
+        this.nombre = nombre,
+        this.precio = precio
+    }
+}
 
-let titulo = "CATÁLOGO DE PRODUCTOS \r\nElige una opción"
-let totalProductos = 5;
-let totalCompra    = 0;
-
-let codProduct1 = 1;
-let codProduct2 = 2;
-let codProduct3 = 3;
-let codProduct4 = 4;
-let codProduct5 = 5;
-
-let descProduct1 = "HAMBURGUESA";
-let descProduct2 = "PIZZA";
-let descProduct3 = "SUSHI";
-let descProduct4 = "TACOS";
-let descProduct5 = "POSTRE";
-
-let precioProduct1 = 120;
-let precioProduct2 = 200;
-let precioProduct3 = 150;
-let precioProduct4 = 80;
-let precioProduct5 = 90;
-
-let nombre = "";
-
-const catalogo = () => {
-
+// recorro el array que contiene el contenido de los productos
+const mostrarCatalogo = () => {
     let mensaje = titulo;
-    mensaje = mensaje + "\r\n";
-    mensaje = mensaje + "\r\n";   
-    mensaje = mensaje + parseInt(codProduct1)+". "+descProduct1+" $"+parseInt(precioProduct1);
-    mensaje = mensaje + "\r\n";
-    mensaje = mensaje + parseInt(codProduct2)+". "+descProduct2+" $"+parseInt(precioProduct2);
-    mensaje = mensaje + "\r\n";
-    mensaje = mensaje + parseInt(codProduct3)+". "+descProduct3+" $"+parseInt(precioProduct3);
-    mensaje = mensaje + "\r\n";
-    mensaje = mensaje + parseInt(codProduct4)+". "+descProduct4+" $"+parseInt(precioProduct4);
-    mensaje = mensaje + "\r\n";
-    mensaje = mensaje + parseInt(codProduct5)+". "+descProduct5+" $"+parseInt(precioProduct5);
-    mensaje = mensaje + "\r\n";
-
+    
+    for (let index = 0; index < arrayProducto.length; index++) {
+        mensaje = mensaje + "\r\n";
+        mensaje = mensaje + parseInt(arrayProducto[index].id)+". "+arrayProducto[index].nombre+" $"+parseFloat(arrayProducto[index].precio);       
+    }
     return (mensaje);
 }
 
-const addCarrito = (producto, cantidad) => { 
-    let mensaje = "";
-
-    switch (producto) {
-        case "1": mensaje = parseInt(codProduct1)+" "+descProduct1+" $"+parseInt(precioProduct1);
-                totalCompra = totalCompra + parseInt (precioProduct1 * cantidad);
-                break;
-        case "2": mensaje = parseInt(codProduct2)+" "+descProduct1+" $"+parseInt(precioProduct2);
-                totalCompra = totalCompra + parseInt (precioProduct2 * cantidad);
-                break;
-        case "3": mensaje = parseInt(codProduct3)+" "+descProduct1+" $"+parseInt(precioProduct3);
-                totalCompra = totalCompra + parseInt (precioProduct3 * cantidad);
-                break;
-        case "4": mensaje = parseInt(codProduct4)+" "+descProduct1+" $"+parseInt(precioProduct4);
-                totalCompra = totalCompra + parseInt (precioProduct4 * cantidad);
-                break;
-        case "5": mensaje = parseInt(codProduct5)+" "+descProduct1+" $"+parseInt(precioProduct5);
-                totalCompra = totalCompra + parseInt (precioProduct5 * cantidad);
-                break;
-
-    }
-
+// Añado producto al Objeto y lo añado al Array
+const addProducto = (producto, precio) => {
+    const productoAdd = new Producto(parseInt (++contadorID),producto,precio)
+    arrayProducto.push (productoAdd)
 }
 
-nombre = prompt ("Ingresa tu nombre por favor");
-let respuesta = "SI";
+nombre = prompt (`Ingresa tu nombre por favor`);
 
-respuesta = prompt ("Hola "+nombre+", quieres comprar en nuestra tienda ? \r\nSI\r\nNO\r\nFIN ");
+do {
+  opcion = prompt (`Hola ${nombre} ¿Quieres grabar productos?\r\nSI\r\nNO`).toUpperCase();
+} while ( (opcion != "SI") && (opcion != "NO") );
 
+    //En este bucle grabo productos usando las funciones para mostrar y grabar
+    while (opcion == "SI") {
 
-while (respuesta == "SI") {
-
-        let producto = prompt (catalogo());
-        let cantidad = prompt ("Qué cantidad deseas "+nombre);
-        addCarrito(producto, cantidad);
-        alert ("Total de tu compra "+parseInt(totalCompra));
-    
-    respuesta = prompt ("Hola "+nombre+", quieres seguir comprando en nuestra tienda ? \r\nSI\r\nNO\r\nFIN ");
-  //  if ((respuesta == "NO") && (totalCompra = 0)) { alert("Necesitamos comprar al menos una vez")}
-
-} 
-
-alert ("Gracias "+nombre+" \r\nEl total de tu compra es $"+parseInt (totalCompra));
+        let producto = prompt ("Descripción del producto");
+        let precio   = prompt ("Precio del producto");
+                        
+            addProducto(producto, precio);
 
 
+        do {
+            opcion = prompt (mostrarCatalogo()+"\r\n____\r\n¿Desea seguir grabando productos?\r\nSI\r\nNO").toUpperCase();
+        } while ( (opcion != "SI") && (opcion != "NO") )
+
+    }
 
 
---
+++ script_actions.js
@@ -0,0 +1,14 @@
+const openModal  = document.querySelector('.action_menu');
+const modal      = document.querySelector('.modal');
+const closeModal = document.querySelector('.modal__close');
+
+
+openModal.addEventListener('click',(e)=>{
+    e.preventDefault();
+    modal.classList.add ('modal--show');
+});
+
+closeModal.addEventListener('click',(e)=>{
+    e.preventDefault();
+    modal.classList.remove ('modal--show');
+});
